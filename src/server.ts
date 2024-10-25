0ed0f0dff3ea119e09e070ba6ebee854b5479fc6
tomoya-kamaji <tomoya.kamaji@gmail.com>
2024-10-26 00:29:50 +0900

    ふりがなUI作成

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import { addFurigana } from "./morphological";
 
+import cors from "cors";
+
 const app = express();
+
+app.use(cors()); // すべてのオリジンを許可
 const port = 5000;
 
 app.get("/api/furigana", async (req, res) => {
   const text = req.query.text as string;
   const furiganaText = await addFurigana(text);
   console.log(furiganaText);
-  
+
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.json({ furiganaText });
 });
